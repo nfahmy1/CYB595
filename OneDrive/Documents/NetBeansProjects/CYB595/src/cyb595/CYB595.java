47d0620d926b52b596db0f1d0e2a08527f476edb
fahmy <fahmy@Mike>
2022-03-19 15:36:56 -0700

    midterm

---
+++ OneDrive/Documents/NetBeansProjects/CYB595/src/cyb595/CYB595.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cyb595;
+
+/**
+ *
+ * @author fahmy
+ */
+public class CYB595 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello CYB595!");
+    }
+    
+}
